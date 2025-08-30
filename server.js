2cdea19bee384774711895561033269427d68da1
Awais-12345 <awaisraza2131@gmail.com>
2025-08-30 15:25:39 -0700

    dffdfdf

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const app = express();
 dotenv.config();
 
 connectDB();
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-
+const allowedOrigin = 'http://localhost:3000' 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: allowedOrigin,
     credentials: true,
   })
 );
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("backened is fully  deployed");});
   
