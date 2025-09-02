8a23be0810284c2b4f36549bef65775afee333f3
Awais-12345 <awaisraza2131@gmail.com>
2025-09-02 03:30:00 -0700

    bvvhhui

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,56 @@
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+
+// exports.protect = async (req, res, next) => {
+//   let token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+
+//   if (!token) {
+//     return res.status(401).json({ message: "Not authorized" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = await User.findById(decoded.id).select("-password");
+//     next();
+//   } catch (err) {
+//     return res.status(401).json({ message: "Token invalid" });
+//   }
+// };
+
+
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
-exports.protect = async (req, res, next) => {
-  let token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Not authorized" });
-  }
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select("-password");
-    next();
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+    });
   } catch (err) {
-    return res.status(401).json({ message: "Token invalid" });
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
   }
 };
